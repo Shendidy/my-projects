a5cfdb4caee481b9c802f873290f28f2f3f320c0
Shendidy <sherif@shendidy.com>
2018-12-28 01:01:32 +0000

    Added more tests and completed the program

--- 0001/lib/test.rb
+++ 0001/lib/test.rb
@@ -1,3 +1,4 @@
 def test(num)
-  num == 0 ? "ZERO" : num.even? ? num + 3 : num + 1
+  num == 0 ? "ZERO" : num.positive? ? num.even? ?
+    num + 3 : num + 1 : num.even? ? num - 3 : num - 1
 end
--- 0001/spec/test_spec.rb
+++ 0001/spec/test_spec.rb
@@ -16,4 +16,16 @@ describe "Converting numbers" do
   it "Return 134 when 133 is passed to it" do
     expect(test(133)).to eq(134)
   end
+  it "Return -2 when -1 is passed to it" do
+    expect(test(-1)).to eq(-2)
+  end
+  it "Return -5 when -2 is passed to it" do
+    expect(test(-2)).to eq(-5)
+  end
+  it "Return -247 when -244 is passed to it" do
+    expect(test(-244)).to eq(-247)
+  end
+  it "Return -134 when -133 is passed to it" do
+    expect(test(-133)).to eq(-134)
+  end
 end
